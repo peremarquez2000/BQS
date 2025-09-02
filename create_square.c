f72c3faac27bc097bf671383f08a7ad49f3d59ac
Pere <peremarquez2000@gmail.com>
2025-09-02 23:26:22 +0200

    Add ft_create_square function

--- create_square.c
+++ create_square.c
@@ -1,13 +1,19 @@
 #include <unistd.h>
 
-
-void	ft_create_square(int dimension, char field, char obstacle)
+void ft_create_square(char **mat, int dimension, char printable_char, char empty_char, char obstacle_char)
 {
-	
-}
+	int checker;
+	int i;
 
-int main()
-{
-	write(1,"a",1);
-
+	checker = 0;
+	i = 0;
+	while (i < dimension)
+	{
+		j = 0;
+		while (j < dimension)
+		{
+			mat[i][j] = printable_char;
+		}
+		i++;
+	}
 }
